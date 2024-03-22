d415d254b96ed67685bfbfb8abb5e704ee015f8a
Fa11enSky <iwanpomirsky@gmail.com>
2024-03-22 02:02:16 +0200

    some filter

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -9,35 +9,49 @@ const CatalogList = () => {
   const [showCounter, setShowCounter] = useState(4);
   const [toShow, setToShow] = useState([]);
   const [isMore, setIsMore] = useState(true);
-  const total = Math.ceil(campers.length / 4);
-  const filters = useSelector(filter)
-  
-const toFilter = campers.filter(item => {
-    return (
-      (!filters.location || item.location === filter.location) &&
-      (!filters.ac || item.details.airConditioner === 1) &&
-      (!filters.automatic || item.transmission === "automatic") &&
-      (!filters.kitchen || item.details.kitchen === 1) &&
-      (!filters.tv || item.details.TV === 1) &&
-      (!filters.bathroom || item.details.bathroom === 1) &&
-      (!filters.van || item.van === 1) &&
-      (!filters.fullyIntegrated || item.details.fullyIntegrated === 1) &&
-      (!filters.alcove || item.form === "alcove")
-    );
-  })
+  const [toFilter, setToFilter] = useState([]);
+  const [total, setTotal] = useState(Math.ceil(campers.length / 4));
 
-
+  const filters = useSelector(filter);
 
   useEffect(() => {
-    if (campers.length === 0) return;
-    const d = campers.filter((_, idx) => {
+    if (Object.values(filters).every((el) => Boolean(el) === false)) {
+      setTotal(Math.ceil(campers.length / 4));
+      setToFilter(campers);
+      setIsMore(true);
+    }
+
+    const filtred = campers.filter((item) => {
+      return (
+        (!filters.location ||
+          item.location.toLowerCase() === filters.location.toLowerCase()) &&
+        (!filters.ac || item.details.airConditioner >= 1) &&
+        (!filters.automatic || item.transmission === "automatic") &&
+        (!filters.kitchen || item.details.kitchen >= 1) &&
+        (!filters.tv || item.details.TV >= 1) &&
+        (!filters.bathroom || item.details.bathroom >= 1) &&
+        (!filters.van || item.form === "panelTruck") &&
+        (!filters.fullyIntegrated || item.form === "fullyIntegrated") &&
+        (!filters.alcove || item.form === "alcove")
+      );
+    });
+    setTotal(Math.ceil(filters.length / 4));
+    setToFilter(filtred);
+    if (toFilter.length <= showCounter) {
+      setIsMore(false);
+    }
+  }, [campers, filters, showCounter, toFilter.length]);
+
+  useEffect(() => {
+    if (toFilter.length === 0) return;
+    const d = toFilter.filter((_, idx) => {
       return idx < showCounter;
     });
     setToShow(d);
-    if (campers.length <= showCounter) {
+    if (toFilter.length <= showCounter) {
       setIsMore(false);
     }
-  }, [campers, showCounter, toShow.length, total]);
+  }, [toFilter, showCounter, toShow.length, total]);
 
   return (
     <div>
