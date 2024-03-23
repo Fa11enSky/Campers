eae208784322b5d93ba3e8901c3d73c6fd1bf11e
Fa11enSky <iwanpomirsky@gmail.com>
2024-03-24 01:00:35 +0200

    update form validation

--- src/components/CamperForm/CamperForm.jsx
+++ src/components/CamperForm/CamperForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import moment from "moment";
 import Calendar from "../Calendar/Calendar";
 import css from "./styles.module.css";
 const CamperForm = () => {
@@ -6,18 +7,40 @@ const CamperForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [comment, setComment] = useState("");
+  
+  const [errors, setErrors] = useState({});
+
   const handleChange = (ev) => {
     if (ev.target.name === "name") setName(ev.target.value);
     if (ev.target.name === "email") setEmail(ev.target.value);
     if (ev.target.name === "comment") setComment(ev.target.value);
   };
+
+  const validateForm = (data) => {
+    let errors = {};
+    if (!data.name.trim()) {
+      errors.name = "User name is required";
+    }
+    if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = "Enter the correct email address";
+    }
+    if (!moment(data.startDate, "DD/MM/YYYY", true).isValid()) {
+      errors.startDate = "Enter the correct date";
+    }
+    return errors;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name.length === 0 || startDate.length === 0 || email.length === 0) {
-      alert("some required field is empty");
-      return;
+    
+    const data = { startDate, email, comment, name };
+    const errors = validateForm(data);
+
+    if (Object.keys(errors).length === 0) {
+      window.location.reload();
+    } else {
+      setErrors(errors);
     }
-    window.location.reload();
   };
 
   return (
@@ -35,6 +58,7 @@ const CamperForm = () => {
         value={name}
         placeholder="Name"
       />
+      {errors.name && <span className="error">{errors.name}</span>}
       <input
         onChange={handleChange}
         value={email}
@@ -44,8 +68,9 @@ const CamperForm = () => {
         type="email"
         placeholder="Email"
       />
+      {errors.email && <span className="error">{errors.email}</span>}
       <Calendar date={startDate} setDate={setStartDate} />
-
+      {errors.startDate && <span className="error">{errors.startDate}</span>}
       <textarea
         onChange={handleChange}
         value={comment}
